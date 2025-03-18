2a074906096159c3345b901bb6369318e5142d5f
Frathol <michaelmosses35746@gmail.com>
2025-03-18 14:25:26 +0700

    menambahkan form pilihan tapi belum dimunculkan dalam tampilan

--- T.js/inter.js
+++ T.js/inter.js
@@ -1,10 +1,25 @@
 document.getElementById("OK").addEventListener("click", function() {
     let nama = document.getElementById("username").value;
     let jumlah = parseInt(document.getElementById("jPilihan").value);
-    let container = document.getElementById("pilihanJumlah");
+    let tempatJ = document.getElementById("pilihanJumlah");
     
     if (!nama || isNaN(jumlah) || jumlah <= 0) {
         alert("Masukkan nama dan jumlah pilihan yang benar!");
         return;
     }
+    let formPilihan = document.createElement("form");
+            formPilihan.id = "formPilihan";
+            
+            for (let i = 1; i <= jumlah; i++) {
+                let label = document.createElement("label");
+                label.textContent = "Pilihan " + i + " :";
+                let input = document.createElement("input");
+                input.type = "text";
+                input.id = "pilihan" + i;
+                formPilihan.appendChild(label);
+                formPilihan.appendChild(input);
+                formPilihan.appendChild(document.createElement("br"));
+            }
+            
+            
 });
